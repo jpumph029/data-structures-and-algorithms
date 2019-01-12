a6d4677a321b6b1c809f68435a3195daf411f3f2
Joseph <pumphrey.joseph@gmail.com>
2019-01-12 12:10:52 -0800

    pictures + tests

--- Challenges/array_binary_search/Program/array_binary_search_Tests/UnitTest1.cs
+++ Challenges/array_binary_search/Program/array_binary_search_Tests/UnitTest1.cs
@@ -6,9 +6,36 @@ namespace array_binary_search_Tests
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void BinaryTest1()
         {
+            //arrange
+            int[] array = new int[] { 4, 8, 15, 16, 23, 42 };
+            int key = 8;
 
+            //act
+
+            //asserted
+            Assert.Equal(1, Program.Program.BinarySearch(array, key));
+        }
+
+
+
+        [Theory]
+        [InlineData(new int[] { 4, 8, 15, 16, 23, 42 }, 15)]
+
+        public void BinaryTest2(int[] arr, int key)
+        {
+            Assert.Equal(2, Program.Program.BinarySearch(arr, key));
+        }
+
+
+
+        [Theory]
+        [InlineData(new int[] { 4, 8, 15, 16, 23, 42 }, 100)]
+
+        public void BinaryTest3(int[] arr, int key)
+        {
+            Assert.Equal(-1, Program.Program.BinarySearch(arr, key));
         }
     }
 }
