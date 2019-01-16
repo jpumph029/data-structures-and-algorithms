0ee8745ca7db7ab5e5280da315342fcfaa67da96
Joseph <pumphrey.joseph@gmail.com>
2019-01-16 11:01:38 -0800

    KthFromEnd method is done, writing tests

--- Challenges/ll_kth_from_end/ll_kth_from_end/Program.cs
+++ Challenges/ll_kth_from_end/ll_kth_from_end/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using LinkedList.Classes;
 
 namespace ll_kth_from_end
 {
@@ -6,7 +7,47 @@ namespace ll_kth_from_end
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            LList list = new LList();
+            list.Insert(1);
+            list.Insert(2);
+            list.Insert(3);
+            list.Insert(4);
+            list.Insert(5);
+            list.Insert(6);
+            list.Insert(7);
+
+            KthFromEnd(3, list);
+        }
+        
+        static int KthFromEnd(int k, LList list)
+        {
+
+            try
+            {
+                Node temp = list.Head;
+                Node Current = list.Head;
+                int counter = 0;
+                while (temp.Next != null)
+                {
+                    temp = temp.Next;
+                    counter++;
+
+                }
+                Current = list.Head;
+                for (int i = 0; i < counter - k; i++)
+                {
+                    Current = Current.Next;
+                }
+                list.Print();
+                Console.WriteLine($"K: {k}\nNode : {Current.Value}");
+                Console.Read();
+                return Current.Value;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            
         }
     }
 }
