9753647604073fbfea5ac54a988284f7e559d109
Joseph <pumphrey.joseph@gmail.com>
2019-01-22 11:02:45 -0800

    a

---
+++ Data Structures/StacksAndQueues/StacksAndQueues/StacksAndQueues/Classes/Queue.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StacksAndQueues.Classes
+{
+    class Queue
+    {
+        public Node Front { get; set; }
+        public Node Rear { get; set; }
+
+        public Queue(Node node)
+        {
+            Front = node;
+            Rear = node;
+
+        }
+    }
+}
