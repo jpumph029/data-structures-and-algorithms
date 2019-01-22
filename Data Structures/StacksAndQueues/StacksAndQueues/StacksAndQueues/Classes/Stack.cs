a8b0ebb2da744f19fd716b87f43688a392aff26e
Joseph <pumphrey.joseph@gmail.com>
2019-01-22 11:13:24 -0800

    Stacks Properties, and constructors and the methods Push, Pop, and Peek are done

--- Data Structures/StacksAndQueues/StacksAndQueues/StacksAndQueues/Classes/Stack.cs
+++ Data Structures/StacksAndQueues/StacksAndQueues/StacksAndQueues/Classes/Stack.cs
@@ -1,4 +1,4 @@
-﻿ckusing System;
+﻿using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +6,52 @@ namespace StacksAndQueues.Classes
 {
     class Stack
     {
+        public Node Top { get; set; }
+
+        /// <summary>
+        /// Stack Constrictor with one Node
+        /// </summary>
+        /// <param name="node"></param>
+        public Stack (Node node)
+        {
+            Top = node;
+        }
+        /// <summary>
+        /// Empty Stack Constructor 
+        /// </summary>
+        public Stack()
+        {
+        }
+        /// <summary>
+        /// Pushes a node to a stack
+        /// </summary>
+        /// <param name="value"></param>
+        public void Push(int value)
+        {
+            Node node = new Node(value);
+            node.Next = Top;
+            Top = node;
+        }
+
+        /// <summary>
+        /// Pops a node off a stack
+        /// </summary>
+        /// <returns></returns>
+        public Node Pop()
+        {
+            Node temp = Top;
+            Top = Top.Next;
+            temp.Next = null;
+            return temp;
+        }
+         
+        /// <summary>
+        /// Peeks at the top of the stack
+        /// </summary>
+        /// <returns></returns>
+        public Node Peek()
+        {
+            return Top;
+        }
     }
 }
