e1d9cda96e4fd9a8561b8607c42fbc8073476c86
Joseph <pumphrey.joseph@gmail.com>
2019-01-23 10:22:54 -0800

    Enqueue tests passed

--- Challenges/queue_with_stacks/QueueWithStacks/UnitTest1.cs
+++ Challenges/queue_with_stacks/QueueWithStacks/UnitTest1.cs
@@ -18,7 +18,19 @@ namespace QueueWithStacks
         {
             PseudoQueue pseudoQueue = new PseudoQueue();
             pseudoQueue.EnQueue(1);
-            Assert.Equal(1, pseudoQueue.PQueue.Top.Value);
+            pseudoQueue.EnQueue(2);
+            pseudoQueue.EnQueue(3);
+            Assert.Equal(3, pseudoQueue.PQueue.Top.Value);
+            Assert.Equal(2, pseudoQueue.PQueue.Top.Next.Value);
+            Assert.Equal(1, pseudoQueue.PQueue.Top.Next.Next.Value);
         }
+        [Fact]
+        public void DoesEnqueueWorkWithNegative()
+        {
+            PseudoQueue pseudoQueue = new PseudoQueue();
+            pseudoQueue.EnQueue(-1);
+            Assert.Equal(-1, pseudoQueue.PQueue.Top.Value);
+        }
+
     }
 }
